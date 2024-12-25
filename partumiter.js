d0bdbc5fe73e4cbf546ab2951237616ce0d83fdd
Harley Hanes <harleyhanes97@gmail.com>
2024-12-24 20:45:35 -0700

    Updated some formatting

--- partumiter.js
+++ partumiter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // Serve static files (HTML, CSS, JS) from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
